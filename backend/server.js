15cb56d5d71ffc3c196d159bd4db0da4130e6795
Uildson S. dos Santos <wilder_shon@hotmail.com>
2025-01-27 21:44:07 -0300

    Adiciona compressão de respostas e logs de erros no  servidor

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const compression = require('compression');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const imageRoutes = require('./routes/imageRoutes');
@@ -8,7 +10,6 @@ const imageRoutes = require('./routes/imageRoutes');
 dotenv.config();
 
 const connectDB = require('./config/db');
-// Conectar ao MongoDB
 connectDB();
 
 const app = express();
@@ -17,12 +18,22 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(compression()); // Compressão de respostas
+app.use(morgan('combined')); // Logs de requisições
+
+// Rotas
 app.use('/api/auth', authRoutes);
 app.use('/api/images', imageRoutes);
 
 // Rota básica
 app.get('/', (req, res) => {
     res.send('Bem-vindo ao backend do Projeto 2!');
+});
+
+// Middleware de erro
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Erro interno no servidor' });
 });
 
 app.listen(PORT, () => {
